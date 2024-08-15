bd445a80f5f473ad393f9a356c555f5754ae42cf
Pipetto-crypto <Pipetto-crypto@pipetto-crypto.com>
2024-08-15 10:40:53 +0200

    wsi: disable two unsupported drm functions when compiling turnip with kgsl

--- src/vulkan/wsi/wsi_common_drm.c
+++ src/vulkan/wsi/wsi_common_drm.c
@@ -46,7 +46,9 @@
 static VkResult
 wsi_dma_buf_export_sync_file(int dma_buf_fd, int *sync_file_fd)
 {
-   return VK_ERROR_FEATURE_NOT_PRESENT;
+   #ifdef HAVE_FREEDRENO_KGSL
+      return VK_ERROR_FEATURE_NOT_PRESENT;
+   #endif
    /* Don't keep trying an IOCTL that doesn't exist. */
    static bool no_dma_buf_sync_file = false;
    if (no_dma_buf_sync_file)
@@ -75,7 +77,9 @@ wsi_dma_buf_export_sync_file(int dma_buf_fd, int *sync_file_fd)
 static VkResult
 wsi_dma_buf_import_sync_file(int dma_buf_fd, int sync_file_fd)
 {
-   return VK_ERROR_FEATURE_NOT_PRESENT;
+   #ifdef HAVE_FREEDRENO_KGSL
+      return VK_ERROR_FEATURE_NOT_PRESENT;
+   #endif
    /* Don't keep trying an IOCTL that doesn't exist. */
    static bool no_dma_buf_sync_file = false;
    if (no_dma_buf_sync_file)
